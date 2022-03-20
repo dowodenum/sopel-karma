ec30e8890bf6fe1132acb2d371bed9096bfd6730
SirVo <57815700+dowodenum@users.noreply.github.com>
2022-03-20 00:13:43 -0700

    Update karma.py
    added duplicate karma check

--- karma.py
+++ karma.py
@@ -4,7 +4,7 @@
 ###
 
 import re
-from sopel.plugin import search, require_admin
+from sopel.plugin import search, require_admin, rate
 from sopel.module import commands, example
 
 KPLUS = "karmaplus"
@@ -15,15 +15,17 @@ nonAdminMSG="This command requires bot admin privileges."
 # using regex instead of a plain command allows for fun things like
 # incrementing karma++ in the start/middle of a line
 # or catching karma changes from users connected via bridge bot
-@plugin.search('(\w+)(\+\+|\-\-)')
-@module.example("<nick>++ to increment, or <nick>-- to decrement, i.e. Sopel++")
+@rate(user=1)
+@search('(\w+)(\+\+|\-\-)')
+@example("<nick>++ to increment, or <nick>-- to decrement, i.e. Sopel++")
 def giveKarma(bot, trigger):
     """Increases/decreases a user's karma - no spaces allowed"""
     nick = trigger.group(1)
     nickdb = nick.lower()
     change = [0,1][trigger.group(2) == '++']
     
-    if (nickdb == trigger.nick.lower()):
+    # command user matches karma target - or multiple karma in single line
+    if (nickdb == trigger.nick.lower()) or trigger.count(['--','++'][change]) > 1 or ('--' in trigger and '++' in trigger):
         bot.reply("Nice try.")
         return
 
@@ -42,17 +44,17 @@ def giveKarma(bot, trigger):
     
     bot.say("%s's karma is now %d" % (nick, karma_val))
 
-@module.commands("karma")
-@module.example(".karma buddha")
+@commands("karma")
+@example(".karma buddha")
 def getKarma(bot, trigger):
     """Display current karma of <nick>"""
 
     nick = trigger.group(2) or trigger.nick
-    nick = nick.lower().strip()
+    nickdb = nick.lower().strip()
     
     try:
-        current_plus = int(bot.db.get_nick_value(nick, KPLUS, 0))
-        current_minus = int(bot.db.get_nick_value(nick, KMINUS, 0))
+        current_plus = int(bot.db.get_nick_value(nickdb, KPLUS, 0))
+        current_minus = int(bot.db.get_nick_value(nickdb, KMINUS, 0))
     except NameError:
         current_plus, current_minus = 0, 0
 
@@ -61,9 +63,9 @@ def getKarma(bot, trigger):
     else:
         bot.say('%s has not gained or lost any karma.' % nick)
 
-@plugin.require_admin(message=nonAdminMSG, reply=True)
-@module.commands("wipe")
-@module.example(".wipe <nick>")
+@require_admin(message=nonAdminMSG, reply=True)
+@commands("wipe")
+@example(".wipekarma <nick>")
 def wipeKarma(bot, trigger):
     """Deletes the karma entries for <nick>"""
     nick = trigger.group(2)
@@ -72,33 +74,35 @@ def wipeKarma(bot, trigger):
         bot.reply('Syntax: Nick contains spaces.')
         return
 
-    nick = nick.lower().strip()
+    nickdb = nick.lower().strip()
 
-    bot.db.delete_nick_value(nick, KPLUS)
-    bot.db.delete_nick_value(nick, KMINUS)
+    bot.db.delete_nick_value(nickdb, KPLUS)
+    bot.db.delete_nick_value(nickdb, KMINUS)
 
     bot.reply('Karma for %s has been wiped.' % nick)
 
-@plugin.require_admin(message=nonAdminMSG, reply=True)
-@module.commands("set")
-@module.example(".set <nick> <incremented> <decremented>")
+@require_admin(message=nonAdminMSG, reply=True)
+@commands("setkarma")
+@example(".setkarma <nick> <incremented> <decremented>")
 def setKarma(bot, trigger):
     """Sets the karma entries for <nick>"""
 
     if trigger.group(2).count(' ') > 2:
         bot.reply('Too many arguments. Syntax: .set <nick> <incremented> <decremented>')
+
     nick = trigger.group(3)
+    nickdb = nick.lower()
     inc = trigger.group(4)
     dec = trigger.group(5)
-
+    
     try:
         inc = int(inc)
         dec = int(dec)
     except ValueError:
         bot.reply("Give integer values for <incremented> and <decremented>, i.e. .set Sopel 69 33")
 
-    bot.db.set_nick_value(nick.lower(), KPLUS, inc)
-    bot.db.set_nick_value(nick.lower(), KMINUS, dec)
+    bot.db.set_nick_value(nickdb, KPLUS, inc)
+    bot.db.set_nick_value(nickdb, KMINUS, dec)
 
     bot.reply('Karma set - nick: %s inc: %s dec: %s' % (nick, inc, dec))
 
